c373e6df621985393a5aa2a0d135efb085c0a679
BACO <wenting150412@gmail.com>
2025-08-11 11:25:18 +0800

    1

--- xlsx2geojson.py
+++ xlsx2geojson.py
@@ -281,4 +281,4 @@ if __name__ == "__main__":
 
 # 因為是在anaconda裡面跑，所以可以直接執行下面這樣
 # & D:/Anaconda/envs/ting/python.exe 執行的這份python檔案路徑 "xlsx檔案名稱" -o "轉換後欲設定之檔案名稱"
-# & D:/Anacoda/envs/ting/python.exe "D:\Ting\javascript\xlsx2geojson.py" "streetlamp_test.xlsx" -o "streetlamp_test.geojson"
+# 範例： & D:/Anacoda/envs/ting/python.exe "D:\Ting\javascript\xlsx2geojson.py" "streetlamp_test.xlsx" -o "streetlamp_test.geojson"
